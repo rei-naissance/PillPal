5ce742eb3fd2ccd5b38ad59fd03eb2e96477b3cf
Philippe Andrei <philippeandreid@gmail.com>
2025-08-03 00:13:01 +0800

    fix: migrated to tailwindcss v3

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
+
+// export default {
+//   plugins: ['@tailwindcss/postcss'],
+// }
